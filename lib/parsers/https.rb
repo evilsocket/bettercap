b30de3823701966d078cd8c8948b1f6adb151477
evilsocket <evilsocket@gmail.com>
2015-04-26 13:18:18 +0200

    HTTPS host parser

---
+++ lib/parsers/https.rb
@@ -0,0 +1,29 @@
+=begin
+
+BETTERCAP
+
+Author : Simone 'evilsocket' Margaritelli
+Email  : evilsocket@gmail.com
+Blog   : http://www.evilsocket.net/
+
+This project is released under the GPL 3 license.
+
+=end
+require_relative 'base'
+require 'colorize'
+require 'resolv'
+
+class HttpsParser < BaseParser
+    def on_packet( pkt )
+        begin
+            if pkt.tcp_dst == 443
+                hostname = Resolv.getname pkt.ip_daddr
+                Logger.write "[#{pkt.ip_saddr}:#{pkt.tcp_src} > #{pkt.ip_daddr}:#{pkt.tcp_dst} #{pkt.proto.last}] " +
+                             "[HTTPS] ".green +
+                             "https://#{hostname}".yellow
+            end
+        rescue
+
+        end
+    end
+end
