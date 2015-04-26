85dc8907193684a31176565f39cf54d177ecf64f
evilsocket <evilsocket@gmail.com>
2015-04-26 12:41:39 +0200

    New mail authentication parser.

---
+++ lib/parsers/mail.rb
@@ -0,0 +1,18 @@
+=begin
+
+BETTERCAP
+
+Author : Simone 'evilsocket' Margaritelli
+Email  : evilsocket@gmail.com
+Blog   : http://www.evilsocket.net/
+
+This project is released under the GPL 3 license.
+
+=end
+require_relative 'base'
+
+class MailParser < BaseParser
+    def initialize
+        @filters = [ /(\d+ )?(auth|authenticate) (login|plain)/i, /(\d+ )?login/i ]
+    end
+end
