3ac8c397042999794506e21734f69d89395fb0b8
evilsocket <evilsocket@gmail.com>
2015-07-21 20:18:01 +0200

    Added header comment to example module.

--- example_proxy_module.rb
+++ example_proxy_module.rb
@@ -1,3 +1,14 @@
+=begin
+
+BETTERCAP
+
+Author : Simone 'evilsocket' Margaritelli
+Email  : evilsocket@gmail.com
+Blog   : http://www.evilsocket.net/
+
+This project is released under the GPL 3 license.
+
+=end
 class HackTitle < Proxy::Module
     def initialize
 
@@ -8,7 +19,7 @@ class HackTitle < Proxy::Module
     end
 
     def on_request request, response
-        # is an html page?
+        # is it a html page?
         if response.content_type == "text/html"
             url = "http://#{request.host}#{request.url}"
             url = url.slice(0..50) + "..." unless url.length <= 50
