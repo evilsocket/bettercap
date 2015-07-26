2a3b8b0a35c66c6d21e686d2cd74be193146953c
Sam Symons <sam@samsymons.com>
2015-07-26 13:15:20 -0700

    Add pcap files for various the base and FTP parsers.
    There has also been some cleaning done of the test files, with helpful methods moved out into a test helper file.

--- test/logger_test.rb
+++ test/logger_test.rb
@@ -1,29 +1,12 @@
 require 'minitest/autorun'
+require 'test_helper'
 require 'logger'
 
 class LoggerTest < MiniTest::Test
   def test_writing_with_a_logfile
-    file = Tempfile.new('bettercap_temp_logfile')
-
-    Logger.logfile = file
-    silence do
+    silence do |output|
       Logger.write 'Test log message'
+      assert_equal output.read, "Test log message\n"
     end
-
-    assert_equal file.read, "Test log message\n"
-    Logger.logfile = nil
-  end
-
-  private
-
-  # Allow for a way to silence calls during test runs.
-  #
-  # This redirects STDOUT to /dev/null for any methods
-  # called inside the block of this method.
-  def silence
-    $stdout = File.new('/dev/null', 'w')
-    yield
-  ensure
-    $stdout = STDOUT
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
 require 'helpers/mock_shell'
+require 'securerandom'
 
 # Override the `shell` private method of the firewall to return the mock
 # shell. The mock shell's purpose is to capture any system calls made and
@@ -9,4 +10,34 @@ def stubbed_firewall(target)
       MockShell
     end
   end
+end
+
+# Allow for a way to silence calls during test runs.
+#
+# This redirects STDOUT to /dev/null for any methods
+# called inside the block of this method.
+def silence(output_file_name = SecureRandom.hex)
+  captured_output = Tempfile.new output_file_name
+  Logger.logfile = captured_output
+
+  $stdout = File.new('/dev/null', 'w')
+  yield captured_output
+ensure
+  $stdout = STDOUT
+end
+
+# Methods for getting PacketFu::Packet objects out of pcap files.
+
+def parsed_packets
+  packets_with_filename 'packets'
+end
+
+def ftp_packets
+  packets_with_filename 'ftp'
+end
+
+private
+
+def packets_with_filename(filename)
+  PacketFu::PcapFile.read_packets(File.join(File.dirname(__FILE__),"pcap/#{filename}.pcap"))
 end
