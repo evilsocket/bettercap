5859be1756c315c77306a4c6c46890433be6bbb7
Felipe Balbi <balbif@gmail.com>
2015-08-05 21:10:37 -0500

    bettercap: add Banner module
    Signed-off-by: Felipe Balbi <balbif@gmail.com>

--- lib/bettercap/version.rb
+++ lib/bettercap/version.rb
@@ -9,7 +9,7 @@ Blog   : http://www.evilsocket.net/
 This project is released under the GPL 3 license.
 
 =end
+
 module BetterCap
   VERSION = '1.1.3'
-  BANNER = File.read( File.dirname(__FILE__) + '/banner' ).gsub( '#VERSION#', "v#{VERSION}")
 end
