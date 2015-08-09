a17ca1fbc9d6a2d58ee260cafaebf00ff4baa86b
evilsocket <evilsocket@gmail.com>
2015-08-09 15:27:26 +0200

    Skipping HTTPS request if the hostname is the same as the previous one.

--- lib/bettercap/sniffer/parsers/https.rb
+++ lib/bettercap/sniffer/parsers/https.rb
@@ -14,23 +14,29 @@ require 'colorize'
 require 'resolv'
 
 class HttpsParser < BaseParser
-    def on_packet( pkt )
-        begin
-          if pkt.tcp_dst == 443
-            # the DNS resolution could take a while and block other parsers.
-            Thread.new do
-              begin
-                  hostname = Resolv.getname pkt.ip_daddr
-              rescue
-                  hostname = pkt.ip_daddr.to_s
-              end
+  @@prev = nil
 
-              Logger.write "[#{pkt.ip_saddr}:#{pkt.tcp_src} > #{pkt.ip_daddr}:#{pkt.tcp_dst} #{pkt.proto.last}] " +
-                           '[HTTPS] '.green +
-                           "https://#{hostname}/".yellow
-            end
+  def on_packet( pkt )
+    begin
+      if pkt.tcp_dst == 443
+        # the DNS resolution could take a while and block other parsers.
+        Thread.new do
+          begin
+              hostname = Resolv.getname pkt.ip_daddr
+          rescue
+              hostname = pkt.ip_daddr.to_s
           end
-        rescue
+
+          if @@prev.nil? or @@prev != hostname
+            Logger.write "[#{pkt.ip_saddr}:#{pkt.tcp_src} > #{pkt.ip_daddr}:#{pkt.tcp_dst} #{pkt.proto.last}] " +
+                         '[HTTPS] '.green +
+                         "https://#{hostname}/".yellow
+
+            @@prev = hostname
+          end
         end
+      end
+    rescue
     end
+  end
 end
