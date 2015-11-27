bf6e83f7488320e9e5c325c8fe9a44a117239879
evilsocket <evilsocket@gmail.com>
2015-11-27 18:40:56 +0100

    More organized and simplified StreamLogger class.

--- lib/bettercap/proxy/stream_logger.rb
+++ lib/bettercap/proxy/stream_logger.rb
@@ -13,28 +13,28 @@ require 'bettercap/logger'
 
 module Proxy
 class StreamLogger
+  @@MAX_REQ_SIZE = 50
+  
+  @@CODE_COLORS  = {
+    '2' => :green,
+    '3' => :light_black,
+    '4' => :yellow,
+    '5' => :red
+  }
+  
   def self.log( is_https, client, request, response )
-    client_s   = "[#{client}]"
-    verb_s     = request.verb
     request_s  = "#{is_https ? 'https' : 'http'}://#{request.host}#{request.url}"
     response_s = "( #{response.content_type} )"
-    request_s  = request_s.slice(0..50) + '...' unless request_s.length <= 50
+    request_s  = request_s.slice(0..@@MAX_REQ_SIZE) + '...' unless request_s.length <= @@MAX_REQ_SIZE
+    code       = response.code[0]
 
-    verb_s = verb_s.light_blue
-
-    if response.code[0] == '2'
-      response_s += " [#{response.code}]".green
-    elsif response.code[0] == '3'
-      response_s += " [#{response.code}]".light_black
-    elsif response.code[0] == '4'
-      response_s += " [#{response.code}]".yellow
-    elsif response.code[0] == '5'
-      response_s += " [#{response.code}]".red
+    if @@CODE_COLORS.has_key? code
+      response_s += " [#{response.code}]".send( @@CODE_COLORS[ code ] )     
     else
-      response_s += " [#{response.code}]"
+      response_s += " [#{response.code}]"      
     end
 
-    Logger.write "#{client_s} #{verb_s} #{request_s} #{response_s}"
+    Logger.write "[#{client}] #{request.verb.light_blue} #{request_s} #{response_s}"
   end
 end
 end
