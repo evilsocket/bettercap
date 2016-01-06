38a90bca4f80b5913a2c9f2d162f2074c75c7211
evilsocket <evilsocket@gmail.com>
2016-01-06 18:17:47 +0000

    Minor refactoring on BetterCap::Discovery::Agents::Udp

--- lib/bettercap/discovery/agents/udp.rb
+++ lib/bettercap/discovery/agents/udp.rb
@@ -19,24 +19,10 @@ class Udp < Base
   private
 
   def send_probe( ip )
-    port = 137
-    message =
-        "\x82\x28\x00\x00\x00" +
-        "\x01\x00\x00\x00\x00" +
-        "\x00\x00\x20\x43\x4B" +
-        "\x41\x41\x41\x41\x41" +
-        "\x41\x41\x41\x41\x41" +
-        "\x41\x41\x41\x41\x41" +
-        "\x41\x41\x41\x41\x41" +
-        "\x41\x41\x41\x41\x41" +
-        "\x41\x41\x41\x41\x41" +
-        "\x00\x00\x21\x00\x01"
-
-    # send netbios udp packet, just to fill ARP table
+    # send dummy udp packet, just to fill ARP table
     sd = UDPSocket.new
-    sd.send( message, 0, ip.to_s, port )
+    sd.send( "\x10\x12\x85\x00\x00", 0, ip.to_s, 137 )
     sd = nil
-    # TODO: Parse response for hostname?
   end
 end
 end
