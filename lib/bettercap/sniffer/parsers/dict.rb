4b10ca61106bc0b75b65a208f3c566fba5e208bf
Brendan Coles <bcoles@gmail.com>
2016-01-27 15:34:37 +0000

    Check destination port is 2628/tcp

--- lib/bettercap/sniffer/parsers/dict.rb
+++ lib/bettercap/sniffer/parsers/dict.rb
@@ -16,8 +16,22 @@ module Parsers
 # DICT authentication parser.
 class Dict < Base
   def initialize
-    @filters = [ /AUTH\s+.+/ ]
     @name = 'DICT'
+  end
+  def on_packet( pkt )
+    begin
+      if pkt.tcp_dst == 2628
+        lines = pkt.to_s.split(/\r?\n/)
+        lines.each do |line|
+          if line =~ /AUTH\s+(.+)\s+(.+)$/
+            user = $1
+            pass = $2
+            StreamLogger.log_raw( pkt, @name, "username=#{user} password=#{pass}" )
+          end
+        end
+      end
+    rescue
+    end
   end
 end
 end
