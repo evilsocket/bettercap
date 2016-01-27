8aecf94b8d948594154bfb4a45e14bcc74a1c477
Brendan Coles <bcoles@gmail.com>
2016-01-27 16:23:38 +0000

    Check destination port is 6379/tcp

--- lib/bettercap/sniffer/parsers/redis.rb
+++ lib/bettercap/sniffer/parsers/redis.rb
@@ -16,8 +16,24 @@ module Parsers
 # Redis authentication parser.
 class Redis < Base
   def initialize
-    @filters = [ /AUTH\s+.+/ ]
     @name = 'REDIS'
+  end
+  def on_packet( pkt )
+    begin
+      if pkt.tcp_dst == 6379
+        lines = pkt.to_s.split(/\r?\n/)
+        lines.each do |line|
+          if line =~ /config\s+set\s+requirepass\s+(.+)$/i
+            pass = "#{$1}"
+            StreamLogger.log_raw( pkt, @name, "password=#{pass}" )
+          elsif line =~ /AUTH\s+(.+)$/i
+            pass = "#{$1}"
+            StreamLogger.log_raw( pkt, @name, "password=#{pass}" )
+          end
+        end
+      end
+    rescue
+    end
   end
 end
 end
