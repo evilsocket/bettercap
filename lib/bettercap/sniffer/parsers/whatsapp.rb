e2b5ceff0afe8d01a4857d00c0b14075d657b787
evilsocket <evilsocket@gmail.com>
2016-02-06 18:12:10 +0100

    Removed useless import from WhatsApp parser.

--- lib/bettercap/sniffer/parsers/whatsapp.rb
+++ lib/bettercap/sniffer/parsers/whatsapp.rb
@@ -15,9 +15,6 @@ This project is released under the GPL 3 license.
 
 =end
 
-require 'bettercap/sniffer/parsers/base'
-require 'colorize'
-
 module BetterCap
 module Parsers
 # HTTP GET requests parser.
