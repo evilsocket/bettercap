9c3038a54d4718c96286c15094efb6978451b323
evilsocket <evilsocket@gmail.com>
2016-02-12 17:11:44 +0100

    Minor fix on SNMP module.

--- lib/bettercap/sniffer/parsers/snmp.rb
+++ lib/bettercap/sniffer/parsers/snmp.rb
@@ -7,8 +7,8 @@ Email  : evilsocket@gmail.com
 Blog   : http://www.evilsocket.net/
 
 SNMP community string parser:
-  Author : Matteo Cantoni 
-  Email  : matteo.cantoni@nothink.org 
+  Author : Matteo Cantoni
+  Email  : matteo.cantoni@nothink.org
 
 This project is released under the GPL 3 license.
 
@@ -23,21 +23,21 @@ class SNMP < Base
   def on_packet( pkt )
     begin
       if pkt.udp_dst == 161
-      
+
         packet = Network::Protos::SNMP::Packet.parse( pkt.payload )
+        unless packet.nil?
+        	if packet.snmp_version_number.to_i == 0
+        	  snmp_version = 'v1'
+        	else
+        	  snmp_version = 'n/a'
+        	end
 
-	if packet.snmp_version_number.to_i == 0
-	  snmp_version = 'v1'
-	else
-	  snmp_version = 'n/a'
-	end
+          msg = "[#{'Version:'.green} #{snmp_version}] [#{'Community:'.green} #{packet.snmp_community_string.map { |x| x.chr }.join.yellow}]"
 
-        msg = "[#{'Version:'.green} #{snmp_version}] [#{'Community:'.green} #{packet.snmp_community_string.map { |x| x.chr }.join.yellow}]"
-
-        StreamLogger.log_raw( pkt, 'SNMP', msg )
+          StreamLogger.log_raw( pkt, 'SNMP', msg )
+        end
       end
-    rescue
-    end
+    rescue; end
   end
 end
 end
