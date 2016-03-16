247ff24983d250f1d327d30708d7acdbedebee10
evilsocket <evilsocket@gmail.com>
2016-03-16 18:14:34 +0100

    Fixes #180: NoMethodError on OS X

--- lib/bettercap/memory.rb
+++ lib/bettercap/memory.rb
@@ -23,33 +23,34 @@ class Memory
 
   def optimize!
     GC.start
+    begin
+      s          = GC.stat
+      new_allocs = s[:total_allocated_objects]
+      new_freed  = s[:total_freed_objects]
+      allocs_d   = nil
+      freed_d    = nil
 
-    s          = GC.stat
-    new_allocs = s[:total_allocated_objects]
-    new_freed  = s[:total_freed_objects]
-    allocs_d   = nil
-    freed_d    = nil
+      if new_allocs < @total_allocs
+        allocs_d = new_allocs.to_s.green
+      elsif new_allocs > @total_allocs
+        allocs_d = new_allocs.to_s.red
+      else
+        allocs_d = new_allocs
+      end
 
-    if new_allocs < @total_allocs
-      allocs_d = new_allocs.to_s.green
-    elsif new_allocs > @total_allocs
-      allocs_d = new_allocs.to_s.red
-    else
-      allocs_d = new_allocs
-    end
+      if new_freed < @total_freed
+        freed_d = new_freed.to_s.red
+      elsif new_freed > @total_freed
+        freed_d = new_freed.to_s.green
+      else
+        freed_d = new_freed
+      end
 
-    if new_freed < @total_freed
-      freed_d = new_freed.to_s.red
-    elsif new_freed > @total_freed
-      freed_d = new_freed.to_s.green
-    else
-      freed_d = new_freed
-    end
+      Logger.debug "GC: allocd objects: #{allocs_d} freed objects: #{freed_d}"
 
-    Logger.debug "GC: allocd objects: #{allocs_d} freed objects: #{freed_d}"
-
-    @total_allocs = new_allocs
-    @total_freed  = new_freed
+      @total_allocs = new_allocs
+      @total_freed  = new_freed
+    rescue; end
   end
 end
 end
