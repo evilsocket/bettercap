93cf44f4a334f2ecd97c936b043b1c73ec131890
evilsocket <evilsocket@gmail.com>
2016-03-17 18:30:58 +0100

    Documentation conf.py file updated.

--- docs/conf.py
+++ docs/conf.py
@@ -170,10 +170,10 @@ html_static_path = ['_static']
 #html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
+html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
 #html_show_copyright = True
