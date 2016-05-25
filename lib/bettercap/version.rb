4b79e836bdaf99d849dd5e1088bc7dcd2a92dd72
evilsocket <evilsocket@gmail.com>
2016-05-25 16:12:06 +0200

    Version bump to 1.5.5

--- lib/bettercap/version.rb
+++ lib/bettercap/version.rb
@@ -12,7 +12,7 @@ This project is released under the GPL 3 license.
 =end
 module BetterCap
   # Current version of bettercap.
-  VERSION = '1.5.5b'
+  VERSION = '1.5.5'
   # Program banner.
   BANNER = File.read( File.dirname(__FILE__) + '/banner' ).gsub( '#VERSION#', "v#{VERSION}")
 end
