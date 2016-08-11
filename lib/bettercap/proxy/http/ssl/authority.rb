564d3bce395aaa544239462cf1a634fe8ded62cb
evilsocket <evilsocket@gmail.com>
2016-08-10 17:14:00 -0700

    Fixed a bug which caused '*.domain.tld' certificates not to be correctly handled

--- lib/bettercap/proxy/http/ssl/authority.rb
+++ lib/bettercap/proxy/http/ssl/authority.rb
@@ -58,6 +58,9 @@ class Store
 
   # Find the +hostname+:+port+ certificate and return it.
   def find( hostname, port )
+    # make sure *.domain.tld hostnames are correctly sanitized
+    hostname.gsub!( "*", "www" )
+
     key = Digest::SHA256.hexdigest( "#{hostname}_#{port}" )
 
     @lock.synchronize {
