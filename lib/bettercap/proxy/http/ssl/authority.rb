58c84eb53cfd12341b436176128340f8d81f4c84
Simone Margaritelli <evilsocket@gmail.com>
2016-08-23 17:11:32 +0200

    Minor fix on wildcard domains handling.

--- lib/bettercap/proxy/http/ssl/authority.rb
+++ lib/bettercap/proxy/http/ssl/authority.rb
@@ -59,7 +59,7 @@ class Store
   # Find the +hostname+:+port+ certificate and return it.
   def find( hostname, port )
     # make sure *.domain.tld hostnames are correctly sanitized
-    hostname.gsub!( "*", "www" )
+    hostname.gsub!( "*.", "www." )
 
     key = Digest::SHA256.hexdigest( "#{hostname}_#{port}" )
 
