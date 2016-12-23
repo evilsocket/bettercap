2dd030ed270c851b97de8da2e7350631b4612ad3
Simone Margaritelli <evilsocket@gmail.com>
2016-12-23 19:30:03 +0100

    Fixes #343: BSD Firewall #del_port_redirection method should selectively remove a rule.

--- lib/bettercap/firewalls/bsd.rb
+++ lib/bettercap/firewalls/bsd.rb
@@ -15,6 +15,10 @@ module BetterCap
 module Firewalls
 # *BSD and OSX Firewall class.
 class BSD < Base
+  def initialize
+    @filename = "/tmp/bettercap_pf_#{Process.pid}.conf"
+  end
+
   # If +enabled+ is true will enable packet forwarding, otherwise it will
   # disable it.
   def enable_forwarding(enabled)
@@ -45,29 +49,47 @@ class BSD < Base
   # Apply the +r+ BetterCap::Firewalls::Redirection port redirection object.
   def add_port_redirection( r )
     # create the pf config file
-    config_file = "/tmp/bettercap_pf_#{Process.pid}.conf"
-
-    File.open( config_file, 'a+t' ) do |f|
-      f.write "rdr pass on #{r.interface} proto #{r.protocol} from any to #{r.src_address.nil? ? 'any' : r.src_address} port #{r.src_port} -> #{r.dst_address} port #{r.dst_port}\n"
+    File.open( @filename, 'a+t' ) do |f|
+      f.write "#{gen_rule(r)}\n"
     end
-
     # load the rule
-    Shell.execute("pfctl -f #{config_file} >/dev/null 2>&1")
+    Shell.execute("pfctl -f #{@filename} >/dev/null 2>&1")
     # enable pf
     enable true
   end
 
   # Remove the +r+ BetterCap::Firewalls::Redirection port redirection object.
   def del_port_redirection( r )
-    # FIXME: This should search for multiple rules inside the
-    # file and remove only this one.
+    # remove the redirection rule from the existing file
+    rule = gen_rule(r)
+    rules = File.readlines(@filename).collect(&:strip).reject(&:empty?)
+    rules.delete(rule)
 
-    # disable pf
-    enable false
-
-    # remove the pf config file
-    File.delete( "/tmp/bettercap_pf_#{Process.pid}.conf" )
+    # no other rules, delete file and disable firewall.
+    if rules.empty?
+      File.delete(@filename)
+      enable false
+    # other rules are present in the file, update it
+    else
+      File.open( @filename, 'w+t' ) do |f|
+        rules.each do |rule|
+          f.write "#{rule}\n"  
+        end
+      end
+      # let the firewall know we updated the file
+      Shell.execute("pfctl -f #{@filename} >/dev/null 2>&1")
+    end
   rescue
+  end
+
+  private
+
+  # Convert +r+ to BSD firewall rule
+  def gen_rule( r )
+    "rdr pass on #{r.interface} " +
+      "proto #{r.protocol} " +
+      "from any to #{r.src_address.nil? ? 'any' : r.src_address} " +
+      "port #{r.src_port} -> #{r.dst_address} port #{r.dst_port}"
   end
 end
 end
