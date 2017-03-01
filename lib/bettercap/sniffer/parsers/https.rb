fb054f559e849d2e30620d4cb268bba78e595329
Simone Margaritelli <evilsocket@gmail.com>
2017-03-02 00:05:32 +0100

    Minor refactoring of HTTPS SNI parser.

--- lib/bettercap/sniffer/parsers/https.rb
+++ lib/bettercap/sniffer/parsers/https.rb
@@ -16,24 +16,26 @@ module Parsers
 # HTTPS connections parser.
 class Https < Base
   @@prev = nil
+  @@lock = Mutex.new
 
   def on_packet( pkt )
     begin
-      if pkt.respond_to?(:tcp_dst)
-        # poor man's TLS Client Hello with SNI extension parser :P
-        if pkt.payload[0] == "\x16" and pkt.payload[1] == "\x03"
-          if pkt.payload =~ /\x00\x00.{4}\x00.{2}([a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6})\x00/
-            hostname = $1
-            if pkt.tcp_dst != 443
-              hostname += ":#{pkt.tcp_dst}"
-            end
-
-            if @@prev.nil? or @@prev != hostname
-              StreamLogger.log_raw( pkt, 'HTTPS', "https://#{hostname}/" )
-              @@prev = hostname
-            end
-          end
+      # poor man's TLS Client Hello with SNI extension parser :P
+      if pkt.respond_to?(:tcp_dst) and \
+         pkt.payload[0] == "\x16" and \
+         pkt.payload[1] == "\x03" and \
+         pkt.payload =~ /\x00\x00.{4}\x00.{2}([a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6})\x00/
+        hostname = $1
+        if pkt.tcp_dst != 443
+          hostname += ":#{pkt.tcp_dst}"
         end
+
+        @@lock.synchronize {
+          if @@prev.nil? or @@prev != hostname
+            StreamLogger.log_raw( pkt, 'HTTPS', "https://#{hostname}/" )
+            @@prev = hostname
+          end
+        }
       end
     rescue; end
   end
