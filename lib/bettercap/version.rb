8fa2b396866bb0117099c484886ddef52bf9b88e
Simone Margaritelli <evilsocket@gmail.com>
2017-06-29 19:43:54 +0200

    Bump to beta

--- lib/bettercap/version.rb
+++ lib/bettercap/version.rb
@@ -12,7 +12,7 @@ This project is released under the GPL 3 license.
 =end
 module BetterCap
   # Current version of bettercap.
-  VERSION = '1.6.1'
+  VERSION = '1.6.2b'
   # Program banner.
   BANNER = File.read( File.dirname(__FILE__) + '/banner' ).gsub( '#VERSION#', "v#{VERSION}")
 end
